36b385db9787a771e6d3a68afc63745078702db5
rondojim <dimronto@gmail.com>
2023-04-12 11:04:37 +0300

    Remove debug

--- test_convex_algos.py
+++ test_convex_algos.py
@@ -6,9 +6,6 @@ def test_graham_scan():
     points = tut.random_point_set(80, 0, 1000)
     convex_hull = gs.Graham_scan(points)
 
-    for p in convex_hull:
-        print(f"{p[0]} {p[1]}")
-
     tut.plot_convex_hull(points, convex_hull)
 
 
